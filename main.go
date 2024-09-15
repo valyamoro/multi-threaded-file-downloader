96a032193d3ccf71151594c66821ca92b00defce
valyamoro <moralek228@gmail.com>
2024-09-15 17:06:10 +0300

    Вынес ссылки в переменные окружения .env

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"github.com/joho/godotenv"
 	"io"
 	"net/http"
 	"os"
+	"strings"
 	"sync"
 )
 
@@ -30,27 +32,45 @@ func downloadFile(url, filepath string) error {
 }
 
 func main() {
-	urls := []struct {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Printf("Ошибка загрузки .env файла: %v\n", err)
+		return
+	}
+
+	urlsEnv := os.Getenv("FILE_URLS")
+	if urlsEnv == "" {
+		fmt.Println("Переменная окружения FILE_URLS не установлена")
+		return
+	}
+
+	urls := strings.Split(urlsEnv, ",")
+	var downloadItems []struct {
 		url      string
 		filename string
-	}{
-		{"http://example.com/file1.txt", "file1.xt"},
-		{"http://example.com/file2.txt", "file2.txt"},
+	}
+	for _, item := range urls {
+		parts := strings.Split(item, "|")
+		if len(parts) != 2 {
+			fmt.Printf("Некорректный формат в строке: %s\n", item)
+			continue
+		}
+		downloadItems = append(downloadItems, struct {
+			url      string
+			filename string
+		}{parts[0], parts[1]})
 	}
 
 	var wg sync.WaitGroup
-
-	for _, file := range urls {
+	for _, file := range downloadItems {
 		wg.Add(1)
-
 		go func(url, filepath string) {
 			defer wg.Done()
 			err := downloadFile(url, filepath)
 			if err != nil {
-				fmt.Printf("Ошибка при загрузке файла: %s: %v\n", filepath, url)
+				fmt.Printf("Ошибка при загрузке файла %s: %v\n", filepath, url)
 				return
 			}
-
 			fmt.Printf("Файл %s успешно загружен\n", filepath)
 		}(file.url, file.filename)
 	}
